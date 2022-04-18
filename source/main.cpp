187c314379cf0473605496104fdead5c17e4a492
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-18 15:02:29 +0700

    main

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
-#include "node.hpp"
+#include <SDL.h>
 
-void PrintNode(Node* &a){
-    while(a != NULL){
-        std::cout<<a->data<<std::endl;
-        a = a->next;
-    }
-}
+
 
 int main(int argc, char* argv[]){
-    Node* a = new Node();
-    Node* b = new Node();
-    a->data = 100;
-    b->data = 111;
-    a->next = b;
-    PrintNode(a);
+    SDL_Window* window = SDL_CreateWindow("Hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 1);
+    SDL_SetRenderDrawColor(renderer, 200, 100, 100, 0);
+    SDL_RenderDrawLine(renderer, 0,0,500,500);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(5000);
     return 0;
 }
