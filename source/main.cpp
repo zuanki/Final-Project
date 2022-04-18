63a21742fd09a8261706714672fd6d35b781a6c6
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-18 15:37:02 +0700

    main

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <SDL.h>
+#include "struct.hpp"
 
 
 
 int main(int argc, char* argv[]){
-    SDL_Window* window = SDL_CreateWindow("Hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 1);
-    SDL_SetRenderDrawColor(renderer, 200, 100, 100, 0);
-    SDL_RenderDrawLine(renderer, 0,0,500,500);
-    SDL_RenderPresent(renderer);
+    App newapp;
+    newapp.window = SDL_CreateWindow("Hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN);
+    newapp.renderer = SDL_CreateRenderer(newapp.window, -1, 1);
+    SDL_SetRenderDrawColor(newapp.renderer, 200, 100, 100, 0);
+    SDL_RenderDrawLine(newapp.renderer, 0,0,500,500);
+    SDL_RenderPresent(newapp.renderer);
     SDL_Delay(5000);
     return 0;
 }
