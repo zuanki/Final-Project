e4e224572d29094e3eba1ec97c22e67e9079b5d1
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-20 12:45:00 +0700

    player

--- include/player.hpp
+++ include/player.hpp
@@ -1,14 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "texture.hpp"
-
-
+#include <vector>
+#include "bullet.hpp"
+enum class Direction {
+    up,
+    down,
+    right,
+    left
+};
 class Player {
     private:
         SDL_Rect des;
         LTexture player;
         float x_pos;
         float y_pos;
+        std::vector<Bullet> bullet;
+        Direction dir;
+        int dx; //move bullet
+        int dy; //move bullet
+        bool is_bullet_move;
+        bool flag;
     public:
         Player();
         ~Player();
@@ -18,9 +30,16 @@ class Player {
         SDL_Rect getRectPlayer(){
             return des;
         }
+        void setXYpos(float u, float v){
+            x_pos = u;
+            y_pos = v;
+        }
+        float getX_pos(){return x_pos;}
+        float getY_pos(){return y_pos;}
         bool loadImagePlayer(std::string path);
         void showImagePlayer();
-        void handleMovePlayer(SDL_Event e);
+        void handleInputPlayer(SDL_Event e);
+        void handleMoveBullet();
 };
 
 #endif 
--- source/player.cpp
+++ source/player.cpp
@@ -8,6 +8,9 @@ Player::Player(){
     des.h = 48;
     x_pos = 13*32;
     y_pos = 10*32;
+    dir = Direction::down;
+    is_bullet_move = false;
+    flag = true;
 }
 Player::~Player(){
     //
@@ -20,15 +23,21 @@ bool Player::loadImagePlayer(std::string path){
     return success;
 }
 void Player::showImagePlayer(){
+    if (is_bullet_move){
+        for (int i=0;i<bullet.size();i++){
+            bullet[i].showBullet();
+        }
+    }
     player.render(x_pos, y_pos, &des);
 }
-void Player::handleMovePlayer(SDL_Event e){
+void Player::handleInputPlayer(SDL_Event e){
     switch (e.key.keysym.sym)
     {
     case SDLK_UP:    
         y_pos -= 8;
         des.y = 48*3;
         des.x+=32;
+        dir = Direction::up;
         if (des.x > 32*3){
             des.x = 0;
         }
@@ -37,6 +46,7 @@ void Player::handleMovePlayer(SDL_Event e){
         y_pos += 8;
         des.y = 0;
         des.x+=32;
+        dir = Direction::down;
         if (des.x > 32*3){
             des.x = 0;
         }
@@ -45,6 +55,7 @@ void Player::handleMovePlayer(SDL_Event e){
         x_pos += 8;
         des.y = 48*2;
         des.x+=32;
+        dir = Direction::right;
         if (des.x > 32*3){
             des.x = 0;
         }
@@ -53,11 +64,76 @@ void Player::handleMovePlayer(SDL_Event e){
         x_pos -= 8;
         des.y = 48*1;
         des.x+=32;
+        dir = Direction::left;
         if (des.x > 32*3){
             des.x = 0;
         }
         break;
+    case SDLK_SPACE:
+        {
+        if (flag){
+            Bullet b; 
+            if (dir==Direction::down){
+                dx = this->getX_pos()-3;
+                dy = this->getY_pos()+32;
+            }
+            else if (dir==Direction::up){
+                dx = this->getX_pos()-3;
+                dy = this->getY_pos()+10;
+            }
+            else if (dir==Direction::left){
+                dx = this->getX_pos()-30;
+                dy = this->getY_pos()+10;
+            }
+            else if (dir==Direction::right){
+                dx = this->getX_pos()+23;
+                dy = this->getY_pos()+13;
+            }
+            is_bullet_move = true;
+            b.setXYpos(dx,dy);
+            bullet.push_back(b);
+            flag = false;
+            }
+        }
+        break;
     default:
         break;
+    }
+}
+void Player::handleMoveBullet(){
+    if (dir==Direction::up){
+        dy-=5;
+        if (dy<y_pos-200){
+            is_bullet_move = false;
+            dy = this->getY_pos();
+            flag = true;
+        }
+    }
+    else if (dir==Direction::down){
+        dy+=5;
+        if (dy>y_pos+200){
+            is_bullet_move = false;
+            dy = this->getY_pos();
+            flag = true;
+        }
+    }
+    else if (dir==Direction::left){
+        dx-=5;
+        if (dx<x_pos-200){
+            is_bullet_move = false;
+            dx = this->getX_pos();
+            flag = true;
+        }
+    }
+    else if (dir==Direction::right){
+        dx+=5;
+        if (dx>x_pos+200){
+            is_bullet_move = false;
+            dx = this->getX_pos();
+            flag = true;
+        }
+    }
+    for (int i=0;i<bullet.size();i++){
+        bullet[i].setXYpos(dx,dy);
     }
 }
