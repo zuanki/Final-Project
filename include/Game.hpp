d98093d796cbaa827e168e64212aac566685ce13
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-22 14:38:39 +0700

    game

---
+++ include/Game.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+class Game{
+private:
+    int cnt =0;
+    bool isRunning = true;
+    SDL_Window* gWindow = NULL;
+
+public:
+    Game();
+    ~Game();
+    bool init();
+    bool loadMedia();
+    void handleEvents();
+    void update();
+    void render();
+    void close();
+
+    bool getGameState(){ return isRunning; }
+
+    static SDL_Renderer* gRenderer;
+
+    //static TTF_Font* gFont;
+};
