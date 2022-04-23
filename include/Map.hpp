6e1f89c9f8d639a0dacd7344d234e3db29fff1c0
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-23 23:22:14 +0700

    map

--- include/Map.hpp
+++ include/Map.hpp
@@ -10,16 +10,19 @@ class Map {
         std::vector<std::string> map;
         Player p;
         std::pair<int, int> move_camera;
+        std::vector<std::pair<int, int>> obstacle;
         SDL_Rect camera;
         LTexture grass;
         LTexture wall;
         LTexture tree;
         LTexture fountain;
+        LTexture water;
     public:
         Map();
         ~Map();
         bool loadMap();
         void handleEvent(SDL_Event e);
+        bool checkCollisions(Direction dir);
         void update();
         void draw();
 };
--- source/Map.cpp
+++ source/Map.cpp
@@ -23,6 +23,9 @@ bool Map::loadMap(){
     if (!this->fountain.loadFromFile(FOUNTAIN_IMAGE_PATH)){
         success = false;
     }
+    if (!this->water.loadFromFile(WATER_IMAGE_PATH)){
+        success = false;
+    }
     if(!p.loadPlayer()){
         success = false;
     }
@@ -40,6 +43,13 @@ bool Map::loadMap(){
     {
         std::cout<<"Can't load file"<<std::endl;
         success = false;
+    }
+    for (int i=7;i<this->map.size()-6;i++){
+        for (int j=9;j<this->map[i].size()-10;j++){
+            if (map[i][j] == '@'){
+                this->obstacle.push_back(std::make_pair(32*j ,32*i));
+            }
+        }
     }
     return success;
 }
@@ -68,16 +78,44 @@ void Map::handleEvent(SDL_Event e){
     switch (e.key.keysym.sym)
     {
     case SDLK_UP:    
-        this->camera.y -= 32;
+        {
+            if (this->checkCollisions(this->p.getDirection())){
+                this->camera.y -= 0;
+            }
+            else{
+                this->camera.y -= 32;
+            }
+        }
         break;
     case SDLK_DOWN:
-        this->camera.y += 32;
+        {
+            if(this->checkCollisions(this->p.getDirection())){
+                this->camera.y += 0;
+            }
+            else {
+                this->camera.y += 32;
+            }
+        }
         break;
     case SDLK_RIGHT:
-        this->camera.x += 32;
+        {
+            if(this->checkCollisions(this->p.getDirection())){
+                this->camera.x += 0;
+            }
+            else {
+                this->camera.x += 32;
+            }
+        }
         break;
     case SDLK_LEFT:
-        this->camera.x -= 32;
+        {
+            if(this->checkCollisions(this->p.getDirection())){
+                this->camera.x -= 0;
+            }
+            else {
+                this->camera.x -= 32;
+            }
+        }
         break;
     default:
         break;
@@ -87,27 +125,6 @@ void Map::draw(){
     SDL_Rect _clip;
     _clip.x = _clip.y = 0;
     _clip.w = _clip.h = SIZE_SMALL;
-    // for (int i=(this->camera.y/32);i<=this->camera.y/32+5;i++){
-    //     for (int j=this->camera.x/32;j<=this->camera.x/32+5;j++){
-    //         switch (map[i][j])
-    //         {
-    //         case '#':
-    //             this->grass.render(SIZE_SMALL*j, SIZE_SMALL*i, &_clip);
-    //             break;
-    //         case '@':
-    //             this->wall.render(SIZE_SMALL*j, SIZE_SMALL*i, &_clip);
-    //             break;
-    //         case '!':
-    //             this->tree.render(SIZE_SMALL*j, SIZE_SMALL*i, &_clip);
-    //             break;
-    //         case '$':
-    //             this->fountain.render(SIZE_SMALL*j, SIZE_SMALL*i, &_clip);
-    //             break;
-    //         default:
-    //             break;
-    //         }
-    //     }
-    // }
     for (int i=0+this->camera.y/32;i<=(this->camera.y/32+SCREEN_HEIGHT/32-1);i++){
         for (int j=0+this->camera.x/32;j<=(this->camera.x/32+SCREEN_WIDTH/32-1);j++){
             switch (this->map[i][j])
@@ -124,10 +141,43 @@ void Map::draw(){
             case '$':
                 this->fountain.render(SIZE_SMALL*j-this->camera.x, SIZE_SMALL*i-this->camera.y, &_clip);
                 break;
+            case '%':
+                this->water.render(SIZE_SMALL*j-this->camera.x, SIZE_SMALL*i-this->camera.y, &_clip);
+                break;
             default:
                 break;
             }
         }
     }
     p.draw();
+}
+bool Map::checkCollisions(Direction dir){
+    bool check = false;
+    for (int i=0;i<this->obstacle.size();i++){
+        if (dir == Direction::up){
+            std::pair<int, int> k = this->p.getPosition();
+            if (k.second == this->obstacle[i].second+32-this->camera.y && k.first == this->obstacle[i].first-this->camera.x){
+                check = true;
+            }
+        }
+        else if (dir == Direction::down){
+            std::pair<int, int> k = this->p.getPosition();
+            if (k.second + 32 == this->obstacle[i].second-this->camera.y && k.first == this->obstacle[i].first-this->camera.x){
+                check = true;
+            }
+        }
+        else if (dir == Direction::left){
+            std::pair<int, int> k = this->p.getPosition();
+            if (k.second == this->obstacle[i].second-this->camera.y && k.first == this->obstacle[i].first + 32 -this->camera.x){
+                check = true;
+            }
+        }
+        else if (dir == Direction::right){
+            std::pair<int, int> k = this->p.getPosition();
+            if (k.second == this->obstacle[i].second-this->camera.y && k.first+32 == this->obstacle[i].first-this->camera.x){
+                check = true;
+            }
+        }
+    }
+    return check;
 }
