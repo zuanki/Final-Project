c253c21dbad17583ffa7f53af3b4161b2bf9997c
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-25 12:28:53 +0700

    main

--- source/main.cpp
+++ source/main.cpp
@@ -1,42 +1,7 @@
 #include <Game.hpp>
-Game *game = NULL;
-
-int main( int argc, char* args[] ){
-	const int FPS = 60;
-	const int frameDelay = 1000/FPS;
-	Uint32 frameStart;
-	int frameTime;
-
-	game = new Game();
-	if(!game->init())
-	{
-		std::cout << "Failed to init window" << std::endl;
-	}
-	else
-	{
-		if(!game->loadMedia())
-		{
-			std::cout << "Failed to load Media" << std::endl;
-		}
-		else
-		{
-			while (game->getGameState())
-			{
-				frameStart = SDL_GetTicks();
-
-				game->handleEvents();
-				game->update();
-				game->render();
-
-				frameTime = SDL_GetTicks() - frameStart;
-
-				if(frameDelay > frameTime){
-					SDL_Delay(frameDelay - frameTime );
-				}
-			}
-		}
-
-	}
-	game->close();
+#include <defs.hpp>
+int main(int argc, char const *argv[])
+{
+	Game(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
 	return 0;
 }
