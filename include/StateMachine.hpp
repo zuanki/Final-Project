e82718268f0df033bc07534ec06dc6792ef3ac56
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-25 12:28:38 +0700

    StateMachine

---
+++ include/StateMachine.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <stack>
+#include <State.hpp>
+
+using StateRef = std::unique_ptr<State>;
+
+class StateMachine {
+public:
+    StateMachine() = default;
+    ~StateMachine() = default;
+
+    void addState(StateRef newState, bool isReplacing = true);
+    void removeState();
+    void processStateChanges();
+
+    StateRef& getActiveState();
+
+private:
+    std::stack<StateRef> states;
+    StateRef newState;
+
+    bool isAdding;
+    bool isReplacing;
+    bool isRemoving;
+};
