c770a2bf4389905420327557cfc3f6617a777fb4
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-26 23:30:11 +0700

    state manager

---
+++ source/StateMachine.cpp
@@ -0,0 +1,36 @@
+#include <StateMachine.hpp>
+
+void StateMachine::addState(StateRef newState, bool isReplacing) {
+    this->isReplacing = isReplacing;
+    this->isAdding = true;
+    this->newState = std::move(newState);
+}
+
+void StateMachine::removeState() {
+    this->isRemoving = true;
+}
+
+void StateMachine::processStateChanges() {
+    if (this->isRemoving && !this->states.empty()) {
+        this->states.pop();
+        if (!this->states.empty()) {
+            this->states.top()->resume();
+        }
+        this->isRemoving = false;
+    }
+    if (this->isAdding) {
+        if (!this->states.empty()) {
+            if (this->isReplacing) {
+                this->states.pop();
+            } else {
+                this->states.top()->pause();
+            }
+        }
+        this->states.push(std::move(this->newState));
+        this->states.top()->init();
+        this->isAdding = false;
+    }
+}
+StateRef& StateMachine::getActiveState() {
+    return this->states.top();
+}
