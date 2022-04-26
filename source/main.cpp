3dbde9ecb3ed6dce847a6f3b418ec6c2e80b7602
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-26 21:00:15 +0700

    main

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,65 @@
-#include <Game.hpp>
-#include <defs.hpp>
-int main(int argc, char const *argv[])
+#include <SDL.h>
+#include <Render/RenderWindow.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[])
 {
-	Game(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
-	return 0;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    RenderWindow window("Test", 500, 500);
+    SDL_Texture *bgTex = IMG_LoadTexture(window.getRenderer(), "../resource/images/art.jpg");
+    Sprite bgSprite(bgTex);
+    SDL_Texture *heroSheet = IMG_LoadTexture(window.getRenderer(), "../resource/images/hero.png");
+    Sprite heroSprite;
+    heroSprite.setTexture(heroSheet);
+    heroSprite.setTextureRect({0, 0, 32, 48});
+    View heroView(window.getDefaultView());
+    int speed = 32;
+    while (window.isOpen())
+    {
+        SDL_Event event;
+        while (window.pollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                window.close();
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.scancode)
+                {
+                case SDL_SCANCODE_W:
+                    heroSprite.move(0, -speed);
+                    break;
+
+                case SDL_SCANCODE_S:
+                    heroSprite.move(0, speed);
+                    break;
+
+                case SDL_SCANCODE_A:
+                    heroSprite.move(-speed, 0);
+                    break;
+
+                case SDL_SCANCODE_D:
+                    heroSprite.move(speed, 0);
+                    break;
+
+                default:
+                    break;
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+        window.clear();
+        heroView.setCenter(heroSprite.getPosition());
+        window.setView(heroView);
+        window.draw(bgSprite);
+        window.draw(heroSprite);
+        window.display();
+    }
+    SDL_DestroyTexture(bgTex);
+    SDL_Quit();
+    return 0;
 }
