711b95635e553e7d33c99bb0cdf1219b88b71332
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-26 23:29:17 +0700

    defs

---
+++ include/Defs.hpp
@@ -0,0 +1,9 @@
+#define TITLE "SkyWalker"
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
+
+//Player 
+#define PLAYER_IMAGE_PATH "../resource/images/hero.png"
+
+//Map
+#define MAP_IMAGE_PATH "../resource/images/art.jpg"
