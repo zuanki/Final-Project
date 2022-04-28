63df63e8d091f31c708674e264a482a9dc0ffcb6
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-28 16:14:23 +0700

    defs

--- include/Defs.hpp
+++ include/Defs.hpp
@@ -1,7 +1,9 @@
 //Screen 
 #define TITLE "SkyWalker"
-#define SCREEN_WIDTH 480
+#define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+#define LEVEL_WIDTH 1280
+#define LEVEL_HEIGHT 960
 
 //Player 
 #define PLAYER_IMAGE_PATH "../resource/images/hero.png"
@@ -10,3 +12,10 @@
 #define MAP_IMAGE_PATH "../resource/images/art.jpg"
 #define MAP_TXT_PATH "../resource/maptypes/0.txt"
 #define MAP_PATH_IMAGE_PATH "../resource/images/SNOW.png" 
+#define MAP_WALL_IMAGE_PATH "../resource/images/BIGSQUARES.png"
+#define MAP_WATER_IMAGE_PATH "../resource/images/WATER.png"
+#define MAP_TREE_IMAGE_PATH "../resource/images/BIGLEAVES.png"
+
+//Bullets 
+#define BULLET_IMAGE_PATH "../resource/images/fire.png"
+#define SHOOTING_SPEED 8
