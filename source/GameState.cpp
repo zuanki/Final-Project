8c591b476559b4b2092fd47d95809d0a6cd76690
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-28 16:15:12 +0700

    game state

--- source/GameState.cpp
+++ source/GameState.cpp
@@ -5,8 +5,14 @@ GameState::GameState(GameDataRef data) : data(data) {
 }
 void GameState::init(){
     this->data->assets.loadTexture("player", PLAYER_IMAGE_PATH);
+    this->data->assets.loadTexture("path_tile", MAP_PATH_IMAGE_PATH);
+    this->data->assets.loadTexture("wall_tile", MAP_WALL_IMAGE_PATH);
+    this->data->assets.loadTexture("tree_tile", MAP_TREE_IMAGE_PATH);
+    this->data->assets.loadTexture("water_tile", MAP_WATER_IMAGE_PATH);
+    this->data->assets.loadTexture("bullet_fire", BULLET_IMAGE_PATH);
     this->player = std::make_unique<Player>(this->data);
     this->map = std::make_unique<Map>(this->data);
+    this->map->init();
 }
 void GameState::handleInput(){
     SDL_Event e;
