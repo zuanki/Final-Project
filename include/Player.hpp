ec0a185a5c1ea0a0661120910254d0dc99d1635d
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-28 16:14:35 +0700

    player

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,16 +1,30 @@
 #pragma once 
 #include <Game.hpp>
+#include <memory>
 #include <Render/Sprite.hpp>
 #include <Utility/Vector2.hpp>
 #include <Render/View.hpp>
+#include <Bullet.hpp>
+
+enum class Direction {
+    up,
+    down, 
+    right,
+    left
+};
+
 class Player {
     private:    
         Sprite sprite_player;
         SDL_Rect player_clip;
+        std::unique_ptr<Bullet> _bullet;
         GameDataRef data;
         View playerView;
+        Direction dir;
+        std::pair<int, int> pos;
+        std::pair<int, int> coor_bullet;
         int moveSpeed;
-        Vector2i direction;
+        int hp = 3;
     public:
         Player(GameDataRef data);
         ~Player();
@@ -18,4 +32,14 @@ class Player {
         void update();
         void draw();
         void setPosition(int u, int v);
+        SDL_Rect getGlobalBounds() {
+            return this->sprite_player.getGlobalBounds();
+        }
+        void takeHit(){
+            --this->hp;
+        }
+        // return current hp
+        int getHP() {
+            return this->hp;
+        }
 };
--- source/Player.cpp
+++ source/Player.cpp
@@ -1,10 +1,14 @@
 #include <Player.hpp>
-
+#include <Defs.hpp>
 Player::Player(GameDataRef data) : data(data){
     this->moveSpeed = 32;
     this->sprite_player.setTexture(this->data->assets.getTexture("player"));
     this->player_clip = {0, 0, 32, 48};
+    this->_bullet = std::make_unique<Bullet>(this->data);
     this->sprite_player.setTextureRect(this->player_clip);
+    this->pos = std::make_pair(32, 32);
+    this->dir = Direction::down;
+    this->coor_bullet = std::make_pair(0, 0);
 }
 Player::~Player(){
     
@@ -14,7 +18,8 @@ void Player::handleInput(SDL_Event e){
         switch (e.key.keysym.scancode)  
         {
         case SDL_SCANCODE_W:
-            this->sprite_player.move(0, -this->moveSpeed);
+            this->dir = Direction::up;
+            this->pos.second -= this->moveSpeed;
             this->player_clip.y = 48*3;
             this->player_clip.x += 32;
             if (this->player_clip.x >= 32*4){
@@ -22,7 +27,8 @@ void Player::handleInput(SDL_Event e){
             }
             break;
         case SDL_SCANCODE_S:
-            this->sprite_player.move(0, this->moveSpeed);
+            this->dir = Direction::down;
+            this->pos.second += this->moveSpeed;
             this->player_clip.y = 48*0;
             this->player_clip.x += 32;
             if (this->player_clip.x >= 32*4){
@@ -30,7 +36,8 @@ void Player::handleInput(SDL_Event e){
             }
             break;
         case SDL_SCANCODE_A:
-            this->sprite_player.move(-this->moveSpeed, 0);
+            this->dir = Direction::left;
+            this->pos.first -= this->moveSpeed;
             this->player_clip.y = 48*1;
             this->player_clip.x += 32;
             if (this->player_clip.x >= 32*4){
@@ -38,11 +45,37 @@ void Player::handleInput(SDL_Event e){
             }
             break;
         case SDL_SCANCODE_D:
-            this->sprite_player.move(this->moveSpeed, 0);
+            this->dir = Direction::right;
+            this->pos.first += this->moveSpeed;
             this->player_clip.y = 48*2;
             this->player_clip.x += 32;
             if (this->player_clip.x >= 32*4){
                 this->player_clip.x = 0;
+            }
+            break;
+        case SDL_SCANCODE_SPACE:
+            {
+                if (this->dir == Direction::down && this->_bullet->getHealth() == 0){
+                    this->coor_bullet = std::make_pair(0, 1);
+                    this->_bullet->setPosition(pos.first, pos.second+32);
+                    this->_bullet->setHealth(1);
+                }
+                else if (this->dir == Direction::up && this->_bullet->getHealth() == 0){
+                    this->coor_bullet = std::make_pair(0, -1);
+                    this->_bullet->setPosition(pos.first, pos.second-32);
+                    this->_bullet->setHealth(1);
+                }
+                else if (this->dir == Direction::left && this->_bullet->getHealth() == 0){
+                    this->coor_bullet = std::make_pair(-1, 0);
+                    this->_bullet->setPosition(pos.first-32, pos.second);
+                    this->_bullet->setHealth(1);
+                }
+                else if (this->dir == Direction::right && this->_bullet->getHealth() == 0){
+                    this->coor_bullet = std::make_pair(1, 0);
+                    this->_bullet->setPosition(pos.first+32, pos.second);
+                    this->_bullet->setHealth(1);
+                }
+                
             }
             break;
         default:
@@ -51,14 +84,23 @@ void Player::handleInput(SDL_Event e){
     }
 }
 void Player::update(){
+    this->_bullet->move(this->coor_bullet);
+    if (this->_bullet->getPosition().first > this->pos.first + 200 || this->_bullet->getPosition().first < this->pos.first-200 || this->_bullet->getPosition().second > this->pos.second +200 || this->_bullet->getPosition().second < this->pos.second -200){
+        this->_bullet->setHealth(0);
+    }
+    this->sprite_player.setPosition(pos.first, pos.second);
     this->sprite_player.setTextureRect(this->player_clip);
     this->playerView = this->data->window.getDefaultView();
     this->playerView.setCenter(this->sprite_player.getPosition());
     this->data->window.setView(this->playerView);
+
 }
 void Player::setPosition(int u, int v){
     this->sprite_player.setPosition(u, v);
 }
 void Player::draw(){
     this->data->window.draw(this->sprite_player);
+    if (this->_bullet->getHealth() == 1){
+        this->_bullet->draw();
+    }
 }
