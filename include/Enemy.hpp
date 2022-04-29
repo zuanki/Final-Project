0fee6d06fea29018c301510ddefc13b2d5d18015
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:11:06 +0700

    enemy

--- include/Enemy.hpp
+++ include/Enemy.hpp
@@ -1,17 +1,19 @@
-#pragma once 
+#pragma once
 #include <Game.hpp>
 #include <Animation.hpp>
 
-class Enemy {
-    private:
-        Sprite sprite;
-        GameDataRef data;
-        std::unique_ptr<Animation> animation;
-        int k = 1;
-    public:
-        Enemy(GameDataRef data);
-        ~Enemy();
-        void moveRandom();
-        void update();
-        void draw();
+class Enemy
+{
+private:
+    Sprite sprite;
+    GameDataRef data;
+    std::unique_ptr<Animation> animation;
+    int k = 1;
+
+public:
+    Enemy(GameDataRef data);
+    ~Enemy();
+    void moveRandom();
+    void update(float deltaTime);
+    void draw();
 };
--- source/Enemy.cpp
+++ source/Enemy.cpp
@@ -1,46 +1,29 @@
 #include <Enemy.hpp>
 #include <Defs.hpp>
-Enemy::Enemy(GameDataRef data) : data(data) {
+Enemy::Enemy(GameDataRef data) : data(data)
+{
     this->sprite.setTexture(this->data->assets.getTexture("mantis_enemy"));
     this->sprite.setTextureRect({0, 0, 32, 32});
     this->sprite.setPosition(-64, -64);
-    this->animation = std::make_unique<Animation>(32*7, 32*4, ENEMY_NUM_FRAMES, ENEMY_FRAMES_TIME);
-    this->animation->setFrameRect(0, 32*0);
+    this->animation = std::make_unique<Animation>(32 * 7, 32 * 4, ENEMY_NUM_FRAMES, ENEMY_FRAMES_TIME);
+    this->animation->setFrameRect(0, 32 * 0);
     this->animation->applyToSprite(this->sprite);
 }
-    
-Enemy::~Enemy(){
+
+Enemy::~Enemy()
+{
     //
 }
-void Enemy::moveRandom(){
-    if (this->sprite.getGlobalBounds().x > 200){
-        k = 2;
-    }
-    else if (this->sprite.getGlobalBounds().x < 32){
-        k = 1;
-    }
-    if (k == 0){
-        this->animation->setFrameRect(0, 0);
-        this->sprite.move(0, 10);
-    }
-    else if (k == 1){
-        this->animation->setFrameRect(0, 32);
-        this->sprite.move(10, 0);
-    }
-    else if (k == 2){
-        this->animation->setFrameRect(0, 32*2);
-        this->sprite.move(-10, 0);
-    }
-    else {
-        this->animation->setFrameRect(0, 32*3);
-        this->sprite.move(0, -10);
-    }
+void Enemy::moveRandom()
+{
 }
-void Enemy::draw(){
+void Enemy::draw()
+{
     this->data->window.draw(this->sprite);
 }
-void Enemy::update(){
-    this->moveRandom();
-    this->animation->update();
+void Enemy::update(float deltaTime)
+{
+    // this->moveRandom();
+    this->animation->update(deltaTime);
     this->animation->applyToSprite(this->sprite);
 }
