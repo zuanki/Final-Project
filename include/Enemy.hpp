d24d42fabdbf6ded944bafbd1c07d15b54e8965f
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 08:22:46 +0700

    enemy

---
+++ include/Enemy.hpp
@@ -0,0 +1,17 @@
+#pragma once 
+#include <Game.hpp>
+#include <Animation.hpp>
+
+class Enemy {
+    private:
+        Sprite sprite;
+        GameDataRef data;
+        std::unique_ptr<Animation> animation;
+        int k = 1;
+    public:
+        Enemy(GameDataRef data);
+        ~Enemy();
+        void moveRandom();
+        void update();
+        void draw();
+};
