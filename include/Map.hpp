40a573cbdfb79b14c044ed9020fbc7a2b662a179
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:11:28 +0700

    map

--- include/Map.hpp
+++ include/Map.hpp
@@ -1,15 +1,23 @@
-#pragma once 
+#pragma once
 #include <Game.hpp>
 #include <Tile.hpp>
 #include <vector>
-class Map {
-    private:    
-        std::vector<std::unique_ptr<Tile>> map;
-        std::vector<std::string> map_text;
-        GameDataRef data;
-    public:
-        Map(GameDataRef data);
-        ~Map();
-        void init();
-        void draw();
+#include <Player.hpp>
+class Map
+{
+private:
+    std::vector<std::unique_ptr<Tile>> map;
+    std::vector<std::string> map_text;
+    std::vector<std::unique_ptr<Tile>> wall;
+    std::unique_ptr<Player> player;
+    GameDataRef data;
+
+public:
+    Map(GameDataRef data);
+    ~Map();
+    void handleInput(SDL_Event e);
+    void update(float deltaTime);
+    void init();
+    void draw();
+    void checkCollisionWithWall();
 };
--- source/Map.cpp
+++ source/Map.cpp
@@ -1,9 +1,12 @@
 #include <Map.hpp>
 #include <Defs.hpp>
 #include <fstream>
-Map::Map(GameDataRef data) : data(data){
+#include <Utility/Collison.hpp>
+Map::Map(GameDataRef data) : data(data)
+{
     std::ifstream readmap(MAP_TXT_PATH);
-    if (readmap.is_open()){
+    if (readmap.is_open())
+    {
         std::string x;
         while (getline(readmap, x))
         {
@@ -11,43 +14,102 @@ Map::Map(GameDataRef data) : data(data){
         }
         readmap.close();
     }
-    for (int i=0;i<this->map_text.size();i++){
-        for (int j=0;j<this->map_text[i].size();j++){
-            if (this->map_text[i][j] == '#'){
+    for (int i = 0; i < this->map_text.size(); i++)
+    {
+        for (int j = 0; j < this->map_text[i].size(); j++)
+        {
+            if (this->map_text[i][j] == '#')
+            {
                 auto k = std::make_unique<Tile>(this->data);
                 k->setTexture(this->data->assets.getTexture("path_tile"));
-                k->setPosition(32*j, 32*i);
+                k->setPosition(32 * j, 32 * i);
                 this->map.push_back(std::move(k));
             }
-            else if (this->map_text[i][j] == '@'){
+            else if (this->map_text[i][j] == '@')
+            {
                 auto k = std::make_unique<Tile>(this->data);
                 k->setTexture(this->data->assets.getTexture("wall_tile"));
-                k->setPosition(32*j, 32*i);
+                k->setPosition(32 * j, 32 * i);
                 this->map.push_back(std::move(k));
             }
-            else if (this->map_text[i][j] == '!'){
+            else if (this->map_text[i][j] == '!')
+            {
                 auto k = std::make_unique<Tile>(this->data);
                 k->setTexture(this->data->assets.getTexture("tree_tile"));
-                k->setPosition(32*j, 32*i);
+                k->setPosition(32 * j, 32 * i);
                 this->map.push_back(std::move(k));
             }
-            else if (this->map_text[i][j] == '$'){
+            else if (this->map_text[i][j] == '$')
+            {
                 auto k = std::make_unique<Tile>(this->data);
                 k->setTexture(this->data->assets.getTexture("water_tile"));
-                k->setPosition(32*j, 32*i);
+                k->setPosition(32 * j, 32 * i);
                 this->map.push_back(std::move(k));
             }
         }
     }
+    for (int i = 0; i < this->map_text.size(); i++)
+    {
+        for (int j = 0; j < this->map_text[i].size(); j++)
+        {
+            if (this->map_text[i][j] == '@')
+            {
+                auto k = std::make_unique<Tile>(this->data);
+                k->setTexture(this->data->assets.getTexture("wall_tile"));
+                k->setPosition(32 * j, 32 * i);
+                this->wall.push_back(std::move(k));
+            }
+        }
+    }
+    this->player = std::make_unique<Player>(this->data);
 }
-Map::~Map(){
-    
-}
-void Map::init(){
+Map::~Map()
+{
     //
 }
-void Map::draw(){
-    for (int i=0;i<this->map.size();i++){
-        map[i]->draw();
+void Map::handleInput(SDL_Event e)
+{
+    this->player->handleInput(e);
+}
+void Map::init()
+{
+    //
+}
+void Map::update(float deltaTime)
+{
+    this->player->update(deltaTime);
+    this->checkCollisionWithWall();
+    // if (Collision::checkCollision(this->player->getGlobalBounds(), this->wall[0]->getGlobalBounds()))
+    // {
+    //     std::cout << "He he it works" << std::endl;
+    // }
+}
+void Map::draw()
+{
+    for (int i = 0; i < this->wall.size(); i++)
+    {
+        wall[i]->draw();
     }
+    this->player->draw();
+}
+void Map::checkCollisionWithWall()
+{
+
+    for (int i = 0; i < this->wall.size(); i++)
+    {
+        if (Collision::checkCollision(this->player->getGlobalBounds(), this->wall[i]->getGlobalBounds()))
+        {
+            this->player->setVelocity(0, 0);
+        }
+    }
+
+    // else if (this->player->getDirection()==Direction::down){
+    //     for (int i = 0; i < this->wall.size(); i++)
+    //     {
+    //         if (Collision::checkCollision(this->player->getGlobalBounds(), this->wall[i]->getGlobalBounds()))
+    //         {
+    //             this->player->setVelocity(0, 0);
+    //         }
+    //     }
+    // }
 }
