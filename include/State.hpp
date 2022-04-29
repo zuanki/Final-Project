d67229e61119871048d0a7d94d8dad11e0148ba1
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:12:28 +0700

    state

--- include/State.hpp
+++ include/State.hpp
@@ -1,10 +1,11 @@
 #pragma once
-class State {
+class State
+{
 public:
     virtual void init() = 0;
     virtual void handleInput() = 0;
-    virtual void update() = 0;
+    virtual void update(float deltaTime) = 0;
     virtual void draw() const = 0;
-    virtual void pause() {};
-    virtual void resume() {};
+    virtual void pause(){};
+    virtual void resume(){};
 };
