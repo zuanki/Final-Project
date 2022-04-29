5ad637f10530b48afddb7cbb6ed8c5792ef34c37
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:09:37 +0700

    animation

--- source/Animation.cpp
+++ source/Animation.cpp
@@ -1,32 +1,36 @@
 #include <Animation.hpp>
 #include <cmath>
 
-Animation::Animation(int sheetSpriteX, int sheetSpriteY, int frameCount, float frameDuration) {
+Animation::Animation(int sheetSpriteX, int sheetSpriteY, int frameCount, float frameDuration)
+{
     this->totalTime = 0.f;
     this->frameDuration = frameDuration;
     this->curFrame = 0;
     this->frameCount = frameCount;
-    this->frameRect.w = sheetSpriteX/ this->frameCount;
-    this->frameRect.h = sheetSpriteY/4;
+    this->frameRect.w = sheetSpriteX / this->frameCount;
+    this->frameRect.h = sheetSpriteY / 4;
     this->frameRect.x = 0;
     this->frameRect.y = 0;
-
 }
 
-void Animation::update() {
-    this->totalTime += 1.f/60.f;
+void Animation::update(float deltaTime)
+{
+    this->totalTime += deltaTime;
 
-    if (this->totalTime > this->frameDuration) {
+    if (this->totalTime > this->frameDuration)
+    {
         this->curFrame += this->totalTime / this->frameDuration;
-        if (this->curFrame >= this->frameCount) {
+        if (this->curFrame >= this->frameCount)
+        {
             this->curFrame %= this->frameCount;
         }
         this->frameRect.x = this->curFrame * this->frameRect.w;
-        
+
         this->totalTime = fmod(this->totalTime, this->frameDuration);
     }
 }
 
-void Animation::applyToSprite(Sprite& sprite) const {
+void Animation::applyToSprite(Sprite &sprite) const
+{
     sprite.setTextureRect(this->frameRect);
 }
