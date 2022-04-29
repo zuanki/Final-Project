09d20f2e8246fe19d81946758c2c4aafa5736a62
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:10:13 +0700

    asset manager

--- include/AssetManager.hpp
+++ include/AssetManager.hpp
@@ -5,17 +5,19 @@
 #include <SDL_image.h>
 #include <iostream>
 
-class AssetManager {
-    public:
-        AssetManager() = default;
-        ~AssetManager();
-        void setRenderer(SDL_Renderer* render){
-            renderer = render;
-        }
-        void loadTexture(const std::string& name, const std::string& filePath);
-        SDL_Texture* getTexture(const std::string& name);
+class AssetManager
+{
+public:
+    AssetManager() = default;
+    ~AssetManager();
+    void setRenderer(SDL_Renderer *render)
+    {
+        renderer = render;
+    }
+    void loadTexture(const std::string &name, const std::string &filePath);
+    SDL_Texture *getTexture(const std::string &name);
 
-    private:
-        std::map<std::string, SDL_Texture*> textures;
-        SDL_Renderer* renderer;
+private:
+    std::map<std::string, SDL_Texture *> textures;
+    SDL_Renderer *renderer;
 };
--- source/AssetManager.cpp
+++ source/AssetManager.cpp
@@ -1,22 +1,25 @@
 #include <AssetManager.hpp>
 
 void AssetManager::loadTexture(const std::string &name, const std::string &filePath)
-{   
-    if (this->textures.find(name) != this->textures.end()) {
+{
+    if (this->textures.find(name) != this->textures.end())
+    {
         return;
     }
 
-    SDL_Texture* newTexture = IMG_LoadTexture(this->renderer, filePath.c_str());
+    SDL_Texture *newTexture = IMG_LoadTexture(this->renderer, filePath.c_str());
     this->textures[name] = newTexture;
 }
 
-SDL_Texture* AssetManager::getTexture(const std::string &name)
+SDL_Texture *AssetManager::getTexture(const std::string &name)
 {
     return this->textures.at(name);
 }
-AssetManager::~AssetManager(){
-    std::map<std::string, SDL_Texture*>::iterator itr;
-    for (itr = this->textures.begin(); itr != this->textures.end(); itr++){
+AssetManager::~AssetManager()
+{
+    std::map<std::string, SDL_Texture *>::iterator itr;
+    for (itr = this->textures.begin(); itr != this->textures.end(); itr++)
+    {
         SDL_DestroyTexture(itr->second);
         itr->second = NULL;
     }
