e137a9d7a687c10bf9ff33023ff171898e62bb80
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:11:13 +0700

    game

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,24 +1,26 @@
-#pragma once 
+#pragma once
 #include <StateMachine.hpp>
 #include <Render/RenderWindow.hpp>
 #include <iostream>
 #include <AssetManager.hpp>
 
 class GameData
-{   
-    public:
-        StateMachine machine;
-        RenderWindow window;
-        AssetManager assets;
+{
+public:
+    StateMachine machine;
+    RenderWindow window;
+    AssetManager assets;
 };
 
 using GameDataRef = std::shared_ptr<GameData>;
 
-class Game {
-    public:
-        Game(int width, int height, std::string title);
-        ~Game();
-    private:
-        GameDataRef data = std::make_shared<GameData>();
-        void run();
+class Game
+{
+public:
+    Game(int width, int height, std::string title);
+    ~Game();
+
+private:
+    GameDataRef data = std::make_shared<GameData>();
+    void run();
 };
--- source/Game.cpp
+++ source/Game.cpp
@@ -1,22 +1,29 @@
 #include <Game.hpp>
 #include <GameState.hpp>
-Game::Game(int width, int height, std::string title){
+Game::Game(int width, int height, std::string title)
+{
     this->data->window.create(title, width, height);
     this->data->assets.setRenderer(this->data->window.getRenderer());
     this->data->machine.addState(StateRef(std::make_unique<GameState>(this->data)));
     this->run();
 }
-void Game::run(){
+void Game::run()
+{
+    float lastTime, curTime, deltaTime;
+    lastTime = SDL_GetTicks() / 1000.f;
     while (this->data->window.isOpen())
     {
+        curTime = SDL_GetTicks() / 1000.f;
+        deltaTime = curTime - lastTime;
+        lastTime = curTime;
         this->data->machine.processStateChanges();
         this->data->machine.getActiveState()->handleInput();
-        this->data->machine.getActiveState()->update();
+        this->data->machine.getActiveState()->update(deltaTime);
         this->data->machine.getActiveState()->draw();
-        SDL_Delay(100);
     }
 }
-Game::~Game(){
-    std::cout<<"ShinoAki"<<std::endl;
+Game::~Game()
+{
+    std::cout << "ShinoAki" << std::endl;
     SDL_Quit();
 }
