a996d7420e19818ed6bf07cbc54d9243397f2aa4
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:10:29 +0700

    bullet

--- include/Bullet.hpp
+++ include/Bullet.hpp
@@ -1,32 +1,31 @@
-#pragma once 
+#pragma once
 #include <Game.hpp>
-#include <utility>
-class Bullet {
-    private:
-        Sprite bullet_sprite;
-        GameDataRef data;
-        std::pair<int, int> move_coor;
-        int health;
-    public:
-        Bullet(GameDataRef data);
-        ~Bullet();
-        void draw();
-        void setMoveCoor(int u, int v){
-            move_coor = std::make_pair(u, v);
-        }
-        void setPosition(int u, int v){
-            bullet_sprite.setPosition(u, v);
-        }
-        void move(std::pair<int, int> q){
-            bullet_sprite.move(q.first, q.second);
-        }
-        void setHealth(int x){
-            health = x;
-        }
-        int getHealth() {
-            return health;
-        }
-        std::pair<int, int> getPosition(){
-            return std::make_pair(this->bullet_sprite.getPosition().x, this->bullet_sprite.getPosition().y);
-        }
+#include <Animation.hpp>
+class Bullet
+{
+    friend class BulletManager;
+
+public:
+    Bullet(GameDataRef data, const Vector2f &direction);
+
+    void update(float deltaTime);
+
+    void draw() const;
+
+    SDL_Rect getGlobalBounds();
+
+    void setPosition(float x, float y);
+
+    void setOut();
+
+private:
+    GameDataRef data;
+
+    Sprite sprite;
+
+    Vector2f direction;
+
+    float moveSpeed;
+
+    bool isOut;
 };
--- source/Bullet.cpp
+++ source/Bullet.cpp
@@ -1,12 +1,43 @@
 #include <Bullet.hpp>
+#include <Defs.hpp>
 
-Bullet::Bullet(GameDataRef data) : data(data) {
-    this->health = 0;
-    this->bullet_sprite.setTexture(this->data->assets.getTexture("bullet_fire"));
+Bullet::Bullet(GameDataRef data, const Vector2f &direction) : data(data), direction(direction)
+{
+
+    this->sprite.setTexture(this->data->assets.getTexture("bullet_fire"));
+    this->moveSpeed = BULLET_MOVE_SPEED;
+    this->isOut = false;
 }
-Bullet::~Bullet(){
-    
+
+void Bullet::update(float deltaTime)
+{
+    auto d = this->direction * this->moveSpeed * deltaTime;
+    // std::cout << d.x << ' ' << d.y << '\n'
+    this->sprite.move(this->direction * this->moveSpeed * deltaTime);
+    // std::cout << this->getGlobalBounds().y << '\n';
+    if (this->getGlobalBounds().y < 0)
+    {
+        this->setOut();
+        std::cout << "Delete \n";
+    }
 }
-void Bullet::draw(){
-    this->data->window.draw(this->bullet_sprite);
+
+void Bullet::setPosition(float x, float y)
+{
+    this->sprite.setPosition(x, y);
+}
+
+void Bullet::setOut()
+{
+    this->isOut = true;
+}
+
+SDL_Rect Bullet::getGlobalBounds()
+{
+    return this->sprite.getGlobalBounds();
+}
+
+void Bullet::draw() const
+{
+    this->data->window.draw(this->sprite);
 }
