fef70e549c6b39886400c4e7970e59871389d335
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-04-29 18:10:52 +0700

    defs

--- include/Defs.hpp
+++ include/Defs.hpp
@@ -1,24 +1,25 @@
-//Screen 
+// Screen
 #define TITLE "SkyWalker"
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-//Player 
+// Player
 #define PLAYER_IMAGE_PATH "../resource/images/hero.png"
 
-//Map
+// Map
 #define MAP_IMAGE_PATH "../resource/images/art.jpg"
 #define MAP_TXT_PATH "../resource/maptypes/0.txt"
-#define MAP_PATH_IMAGE_PATH "../resource/images/SNOW.png" 
+#define MAP_PATH_IMAGE_PATH "../resource/images/SNOW.png"
 #define MAP_WALL_IMAGE_PATH "../resource/images/BIGSQUARES.png"
 #define MAP_WATER_IMAGE_PATH "../resource/images/WATER.png"
 #define MAP_TREE_IMAGE_PATH "../resource/images/BIGLEAVES.png"
 
-//Bullets 
+// Bullets
 #define BULLET_IMAGE_PATH "../resource/images/fire.png"
-#define SHOOTING_SPEED 8
+#define BULLET_MOVE_SPEED 1000.f
+#define BULLET_SHOOTING_COOLDOWN 0.3f
 
-//Enemies
+// Enemies
 #define MANTIS_IMAGE_PATH "../resource/images/Enemies/MantisAttack.png"
 #define ENEMY_NUM_FRAMES 7
-#define ENEMY_FRAMES_TIME 1.f/12.f
+#define ENEMY_FRAMES_TIME 1.f / 12.f
