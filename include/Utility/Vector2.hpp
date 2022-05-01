6f08b1f998aab9d3264e14be73d894727e2fa147
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-05-01 20:33:48 +0700

    vector2

--- include/Utility/Vector2.hpp
+++ include/Utility/Vector2.hpp
@@ -9,6 +9,57 @@ struct Vector2
     {
         return Vector2(x * k, y * k);
     }
+    Vector2 operator-() const
+    {
+        return Vector2(-this->x, -this->y);
+    }
+
+    Vector2 &operator+=(const Vector2 &other)
+    {
+        this->x += other.x;
+        this->y += other.y;
+        return *this;
+    }
+
+    Vector2 &operator-=(const Vector2 &other)
+    {
+        *this += -other;
+        return *this;
+    }
+
+    Vector2 &operator*=(float k)
+    {
+        this->x *= k;
+        this->y *= k;
+
+        return *this;
+    }
+
+    Vector2 &operator/=(float k)
+    {
+        this->x *= 1.f / k;
+        return *this;
+    }
+
+    Vector2 operator+(const Vector2 &other) const
+    {
+        return Vector2(this->x + other.x, this->y + other.y);
+    }
+
+    Vector2 operator-(const Vector2 &other) const
+    {
+        return *this + (-other);
+    }
+
+    Vector2 operator*(float k) const
+    {
+        return Vector2(this->x * k, this->y * k);
+    }
+
+    Vector2 operator/=(float k) const
+    {
+        return (*this) * (1.f / k);
+    }
 };
 
 using Vector2f = Vector2<float>;
