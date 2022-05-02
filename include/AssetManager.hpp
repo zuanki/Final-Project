305e0cc110c8d62a4f0b1ac5bb8ed9f1913dece5
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-05-02 21:17:13 +0700

    asset manager

--- include/AssetManager.hpp
+++ include/AssetManager.hpp
@@ -4,7 +4,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <iostream>
-
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 class AssetManager
 {
 public:
@@ -16,8 +17,23 @@ public:
     }
     void loadTexture(const std::string &name, const std::string &filePath);
     SDL_Texture *getTexture(const std::string &name);
+    void loadFont(const std::string &name, const std::string &filePath, int size);
+    TTF_Font *getFont(const std::string &name);
+    void loadMusic(const std::string &filePath);
+    Mix_Music *getMusic()
+    {
+        return this->music;
+    }
+    void loadChuck(const std::string &filePath);
+    Mix_Chunk *getChuck()
+    {
+        return this->bullet;
+    }
 
 private:
     std::map<std::string, SDL_Texture *> textures;
+    std::map<std::string, TTF_Font *> fonts;
+    Mix_Music *music;
+    Mix_Chunk *bullet;
     SDL_Renderer *renderer;
 };
--- source/AssetManager.cpp
+++ source/AssetManager.cpp
@@ -11,9 +11,31 @@ void AssetManager::loadTexture(const std::string &name, const std::string &fileP
     this->textures[name] = newTexture;
 }
 
+void AssetManager::loadFont(const std::string &name, const std::string &filePath, int size)
+{
+    if (this->fonts.find(name) != this->fonts.end())
+    {
+        return;
+    }
+    TTF_Font *newFont = TTF_OpenFont(filePath.c_str(), size);
+    this->fonts[name] = newFont;
+}
+
 SDL_Texture *AssetManager::getTexture(const std::string &name)
 {
     return this->textures.at(name);
+}
+TTF_Font *AssetManager::getFont(const std::string &name)
+{
+    return this->fonts.at(name);
+}
+void AssetManager::loadMusic(const std::string &filePath)
+{
+    this->music = Mix_LoadMUS(filePath.c_str());
+}
+void AssetManager::loadChuck(const std::string &filePath)
+{
+    this->bullet = Mix_LoadWAV(filePath.c_str());
 }
 AssetManager::~AssetManager()
 {
@@ -23,6 +45,16 @@ AssetManager::~AssetManager()
         SDL_DestroyTexture(itr->second);
         itr->second = NULL;
     }
+    std::map<std::string, TTF_Font *>::iterator i;
+    for (i = this->fonts.begin(); i != this->fonts.end(); i++)
+    {
+        TTF_CloseFont(i->second);
+        i->second = NULL;
+    }
+    Mix_FreeMusic(this->music);
+    this->music = NULL;
+    Mix_FreeChunk(this->bullet);
+    this->bullet = NULL;
     SDL_DestroyRenderer(this->renderer);
     this->renderer = NULL;
 }
