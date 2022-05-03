ff5ab7e383a30d9cbf87d299cdda5100c76a9ca7
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-05-03 20:35:55 +0700

    deleted

--- source/TextObject.cpp
+++
@@ -1,9 +0,0 @@
-#include <TextObject.hpp>
-
-TextObject::TextObject()
-{
-    this->color = {255, 255, 255};
-    this->textSurface = TTF_RenderText_Solid(font, content.c_str(), color);
-    this->textTexture = SDL_CreateTextureFromSurface(this->renderer, this->textSurface);
-    this->text.setTexture(this->textTexture);
-}
