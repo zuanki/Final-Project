0619f8878b3316a31c9ea216be0fed04d5500970
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-05-06 14:34:43 +0700

    state

--- include/State.hpp
+++ include/State.hpp
@@ -5,7 +5,7 @@ public:
     virtual void init() = 0;
     virtual void handleInput() = 0;
     virtual void update(float deltaTime) = 0;
-    virtual void draw() const = 0;
+    virtual void draw() = 0;
     virtual void pause(){};
     virtual void resume(){};
 };
