244a1b7958dee25f4eef734cbdad5e149fab4827
zuanki <nguyenxuanhoakhtn@gmai.com>
2022-05-09 17:54:25 +0700

    defs

--- include/Defs.hpp
+++ include/Defs.hpp
@@ -21,11 +21,12 @@
 
 // Enemies
 #define MANTIS_IMAGE_PATH "../resource/images/Enemies/MantisAttack.png"
+#define BEETLE_IMAGE_PATH "../resource/images/Enemies/BeetleAttack.png"
 #define ENEMY_NUM_FRAMES 7
 #define ENEMY_FRAMES_TIME 1.f / 12.f
 
 // Fonts
-#define FONT_PATH "../resource/fonts/EnterCommand.ttf"
+#define FONT_PATH "../resource/fonts/NewFont.ttf"
 
 // Audios
 #define MUSIC_PATH "../resource/audios/music.wav"
